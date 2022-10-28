ea6f7fd8fe639fd3f7658d5d2990e66c2c5a474b
milad <miladmohammadi.a27@gmail.com>
2022-10-28 15:37:21 +0330

    Done: selling models and register to admin

--- selling/admin.py
+++ selling/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import Stadium, Team, Match
 
 @admin.register(Stadium)
-class ServicesAdmin(admin.ModelAdmin):
+class StadiumsAdmin(admin.ModelAdmin):
     list_display = ('name', 'stadium_id')
 
 @admin.register(Team)
-class ServicesAdmin(admin.ModelAdmin):
+class TeamsAdmin(admin.ModelAdmin):
     list_display = ('name', 'team_id')
 
 @admin.register(Match)
-class ServicesAdmin(admin.ModelAdmin):
-    list_display = ('match_id', 'home_team', 'guest_team', 'stadium', 'datetime')
+class MatchesAdmin(admin.ModelAdmin):
+    list_display = ('home_team', 'guest_team', 'stadium', 'datetime')
--- selling/models.py
+++ selling/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from uuid import uuid4
 from jsonfield import JSONField
-from django.db.models.signals import pre_save
 from django.shortcuts import get_object_or_404
 
+"""
+the selling app purpose is :
+1- define stadiums for matches --> admins should do this
+2- define teams for playing against each other --> admins should do this
+3- define matches between two teams and in the one of defined stadiums --> admins should do this
+4- view and buy seats by users to watch the match --> users should do this
+"""
 
 class Stadium(models.Model):
     name = models.CharField(max_length=50, verbose_name='Stadium Name')
     stadium_id = models.UUIDField(primary_key=True, editable=False, default=uuid4,
                                   unique=True, verbose_name='Stadium ID')
+
+    # all available seats id or name, in the stadium should entered here as a list
     seats = JSONField(verbose_name='Stadium Seats', default={
         "seats": []
     }
@@ -36,13 +44,19 @@ class Team(models.Model):
 
 class Match(models.Model):
     stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE, verbose_name='Stadium')
-    match_id = models.UUIDField(primary_key=True, editable=False, default=uuid4, unique=True, verbose_name='Match ID')
     home_team = models.ForeignKey(Team, on_delete=models.CASCADE, verbose_name='Home Team',
                                   related_name='match_home_team')
     guest_team = models.ForeignKey(Team, on_delete=models.CASCADE, verbose_name='Guest Team',
                                    related_name='match_guest_team')
     datetime = models.DateTimeField()
-    sold_tickets = JSONField(verbose_name='Sold Tickets', editable=True)
+
+    """
+    a key value dictionary that represent :
+    key ---> seat id or seat name 
+    value ---> the user id for user that reserved the seat if None it means the seat is not reserved yet
+    this field will be automatic generated based on stadium seats list when a new match defines
+    """
+    seats_reserve = JSONField(verbose_name='Seats And Reserving Status', editable=True)
 
     class Meta:
         verbose_name = 'Match'
@@ -50,16 +64,19 @@ class Match(models.Model):
 
     def __str__(self):
         return 'Match Between {} VS {} In {} Stadium At {}'.format(
-            self.stadium,
             self.home_team,
             self.guest_team,
+            self.stadium,
             self.datetime,
         )
 
-def match_pre_save(*args, **kwargs):
-    stadium: Stadium = get_object_or_404(Stadium, stadium_id=kwargs.get('instance').stadium.stadium_id)
-    seats_list: list = stadium.seats['seats']
-    kwargs.get('instance').sold_tickets = dict.fromkeys(seats_list)
+    # when a new match going to created seats_reserve field generated based on selected stadium seats list
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
 
+        if not self.pk:
+            stadium: Stadium = get_object_or_404(Stadium, stadium_id=self.stadium.stadium_id)
+            seats_list: list = stadium.seats['seats']
+            self.seats_reserve = dict.fromkeys(seats_list)
+        super().save()
 
-pre_save.connect(match_pre_save, sender=Match)
